63d856e7bf635c8c53882b34dac8caacb5e10627
dkov88 <dkov1988@gmail.com>
2015-02-23 00:28:48 +0200

    change form

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -8,18 +8,20 @@ class BudgetsController < ApplicationController
     filtering_params(params).each do |key, value|
       @budgets = @budgets.public_send(key, value) if value.present?
     end
-    
   end
 
   def new
     @budget = Budget.new
+    @type = budget_type
   end
 
   def create
     @budget = Budget.create(budget_params)
+
   end
 
   def edit
+    @type = budget_type
     @budget = Budget.find(params[:id])
   end
 
@@ -54,4 +56,14 @@ private
   def filtering_params(params)
     params.slice(:description, :money_kind, :start_date, :end_date)
   end
+
+
+  def budget_type
+    wallet = Wallet.find_by_email(current_user.email)
+    array ||= []
+    array << ['income', 'income'] if (wallet.income == true)
+    array << ['expense', 'expense'] if (wallet.expense == true)
+    return array
+  end
+
 end
